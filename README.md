f05c76c1051d9e302cd3acccd77e108392c5630d
Fedor <fifero@mail.ru>
2024-10-10 18:59:28 +0300

    build(test): setup test configuration

---
+++ README.md
@@ -0,0 +1,20 @@
+# Lumin page builder
+
+## Project Setup
+
+### Installing dependencies
+```sh
+npm install
+```
+
+### Start the server, which will automatically restart when files are changed
+
+```sh
+npm run dev
+```
+
+### Build
+
+```sh
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+</template>
+
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped>
+
+</style>
