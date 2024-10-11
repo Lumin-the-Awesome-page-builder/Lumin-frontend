86c96100e7dc113e1742808a10a524b32543bbe0
dudosyka <a.hatson22@gmail.com>
2024-10-11 20:13:18 +0300

    build(test): setup vue test suit

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from 'vitest/config'
+import { fileURLToPath, URL } from 'node:url';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+    plugins: [vue()],
     test: {
-        coverage: {
-            reportsDirectory: './test/coverage',
-            reporter: ['text-summary']
-        },
+      environment: 'jsdom',
+      coverage: {
+        provider: "v8",
+        reportsDirectory: './test/coverage',
+        reporter: ['text', 'text-summary']
+      },
+    },
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
+      },
     },
 })
