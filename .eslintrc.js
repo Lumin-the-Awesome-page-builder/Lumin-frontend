699bf11991363d895b0362a3b54ea4af847aec3c
Fedor <fifero@mail.ru>
2024-10-25 17:53:09 +0300

    build(lint): update eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -23,6 +23,8 @@ module.exports = {
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-unsafe-function-type': 'off',
+    '@typescript-eslint/ban-ts-comment': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
