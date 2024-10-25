652bc0a149a9f36af08f5c7061aaf913c4b4f5a0
Fedor <fifero@mail.ru>
2024-10-25 18:56:42 +0300

    feat(api): api-model, api-dto, api-conf, token, user-dto, logger
    LoggerUtil for logs management;
    TokenUtil for working with user tokens;
    ApiModelUtil class for working with api requests with auto refresh for tokens.

---
+++ src/api/conf/app.conf.example.ts
@@ -0,0 +1,30 @@
+export const serverConf = {
+  proto: '',
+  endpoint: '',
+  refreshEndpoint: '',
+  transferInProgressStatus: 0,
+  wsEndpoint: '',
+  loggerMode: '',
+  loggerExcludedPrefixes: [],
+  rules: {
+    //Key-Value
+  },
+};
+
+export enum TransactionType {
+  INCOME = 1,
+  OUTCOME,
+  TRANSFER,
+}
+export enum TransactionStatus {
+  CREATED = 1,
+  OPEN,
+  CREATION_CANCELLED,
+  IN_PROGRESS,
+  PROCESSING_CANCELLED,
+  DELIVERED,
+  NOT_DELIVERED,
+  DELIVERY_CANCELLED,
+  FAILED,
+  PROCESSED,
+}
