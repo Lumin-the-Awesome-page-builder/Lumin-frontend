97d329eda6806871bdce56e3b438a2bdbbc6bb71
h1l3xx <alksmlkv@gmail.com>
2024-10-28 01:16:09 +0300

    test(components): added tests to check the available props

--- test/editor/components/header.test.ts
+++ test/editor/components/header.test.ts
@@ -1,5 +1,11 @@
 import { describe, expect, it } from 'vitest';
 import Header from '@/editor/components/Header.ts';
+import FontWeightProp from '@/editor/properties/text/FontWeightProp.ts';
+import InlineTextProps from '@/editor/properties/text/InlineTextProps.ts';
+import LeadParagraphProp from '@/editor/properties/text/LeadParagraphProp.ts';
+import MonoSpaceProp from '@/editor/properties/text/MonoSpaceProp.ts';
+import TextAlignProp from '@/editor/properties/text/TextAlignProp.ts';
+import TextTransformProp from '@/editor/properties/text/TextTransformProp.ts';
 
 describe('Test header component', () => {
   it('Test component creation', () => {
@@ -8,5 +14,17 @@ describe('Test header component', () => {
 
       expect(header.elementName).toBe(`h${level}`);
     });
+  });
+
+  it('Test available component props', () => {
+    const header = new Header(1);
+    expect(header.availableProps).toStrictEqual([
+      FontWeightProp.name,
+      InlineTextProps.name,
+      LeadParagraphProp.name,
+      MonoSpaceProp.name,
+      TextAlignProp.name,
+      TextTransformProp.name,
+    ]);
   });
 });
--- test/editor/components/link.test.ts
+++ test/editor/components/link.test.ts
@@ -1,10 +1,36 @@
 import { describe, expect, it } from 'vitest';
 import Link from '@/editor/components/Link.ts';
+import FontWeightProp from '@/editor/properties/text/FontWeightProp.ts';
+import InlineTextProps from '@/editor/properties/text/InlineTextProps.ts';
+import LeadParagraphProp from '@/editor/properties/text/LeadParagraphProp.ts';
+import MonoSpaceProp from '@/editor/properties/text/MonoSpaceProp.ts';
+import TextAlignProp from '@/editor/properties/text/TextAlignProp.ts';
+import TextTransformProp from '@/editor/properties/text/TextTransformProp.ts';
+import LinkOpacityProp from '@/editor/properties/link/LinkOpacityProp.ts';
+import LinkUnderlineProp from '@/editor/properties/link/LinkUnderlineProp.ts';
+import LinkUnderlineOpacity from '@/editor/properties/link/LinkUnderlineOpacity.ts';
+import LinkUnderlineOffsetProp from '@/editor/properties/link/LinkUnderlineOffsetProp.ts';
 
 describe('Test link component', () => {
   it('Test component creation', () => {
     const link = new Link('');
 
     expect(link.elementName).toBe('a');
+  });
+
+  it('Test available component props', () => {
+    const link = new Link('');
+    expect(link.availableProps).toStrictEqual([
+      LinkOpacityProp.name,
+      LinkUnderlineProp.name,
+      LinkUnderlineOpacity.name,
+      LinkUnderlineOffsetProp.name,
+      FontWeightProp.name,
+      InlineTextProps.name,
+      LeadParagraphProp.name,
+      MonoSpaceProp.name,
+      TextAlignProp.name,
+      TextTransformProp.name,
+    ]);
   });
 });
