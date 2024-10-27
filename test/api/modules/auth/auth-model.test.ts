46b5e6d30ddb86ca6250320d711311e6c8d88676
Fedor <fifero@mail.ru>
2024-10-27 22:56:39 +0300

    test(auth-model): unit tests for AuthModel and related dto

---
+++ test/api/modules/auth/auth-model.test.ts
@@ -0,0 +1,116 @@
+import { beforeEach, afterEach, describe, expect, it, vi } from 'vitest';
+import AuthModel from '@/api/modules/auth/models/auth.model.ts';
+import AuthInputDto from '@/api/modules/auth/dto/login/auth-input.dto.ts';
+import ApiResponseDto from '@/api/dto/api-response.dto.ts';
+import ApiRequestDto from '@/api/dto/api-request.dto.ts';
+import TokenUtil from '@/utils/token.util.ts';
+
+vi.mock('@/utils/token.util', () => {
+  return {
+    default: {
+      setAuthorized: vi.fn(),
+      login: vi.fn(),
+    },
+  };
+});
+
+describe('Base AuthModel class tests', () => {
+  let authModel: AuthModel;
+  beforeEach(() => {
+    authModel = new AuthModel();
+  });
+
+  it('Test authModel creation', () => {
+    expect(authModel).toEqual({
+      baseEndpoint: '/auth',
+      baseEndpointBuffer: '',
+      onRefresh: null,
+    });
+  });
+
+  describe('Test auth', async () => {
+    let authInputDto: AuthInputDto;
+    let apiRequestDto: ApiRequestDto;
+    beforeEach(() => {
+      authInputDto = new AuthInputDto('login', 'password');
+      apiRequestDto = new ApiRequestDto('', 'POST', authInputDto);
+    });
+    afterEach(() => {
+      vi.restoreAllMocks();
+    });
+
+    it('Token api response success', async () => {
+      const data = 'data';
+      const apiResponseDto = new ApiResponseDto(true, data, null);
+      authModel.unauthorizedRequest = vi.fn(
+        () =>
+          new Promise<ApiResponseDto<any>>((resolve) => {
+            resolve(apiResponseDto);
+          }),
+      );
+      //@ts-ignore
+      authModel.requestAuthorizedData = vi.fn(
+        () =>
+          new Promise<ApiResponseDto<any>>((resolve) => {
+            resolve(apiResponseDto);
+          }),
+      );
+
+      const result = await authModel.auth(authInputDto);
+
+      expect(result).toEqual({ ...apiResponseDto });
+      expect(authModel.unauthorizedRequest).toBeCalledWith(apiRequestDto);
+      //@ts-ignore
+      expect(TokenUtil.login).toBeCalledWith(data);
+      //@ts-ignore
+      expect(authModel.requestAuthorizedData).toBeCalledTimes(1);
+      //@ts-ignore
+      expect(TokenUtil.setAuthorized).toBeCalledWith(data);
+    });
+
+    it('Token api response failed', async () => {
+      const data = 'data';
+      const apiResponseDto = new ApiResponseDto(false, data, null);
+      authModel.unauthorizedRequest = vi.fn(
+        () =>
+          new Promise<ApiResponseDto<any>>((resolve) => {
+            resolve(apiResponseDto);
+          }),
+      );
+      //@ts-ignore
+      authModel.requestAuthorizedData = vi.fn(
+        () =>
+          new Promise<ApiResponseDto<any>>((resolve) => {
+            resolve(apiResponseDto);
+          }),
+      );
+
+      const result = await authModel.auth(authInputDto);
+
+      expect(result).toEqual({ ...apiResponseDto });
+      expect(authModel.unauthorizedRequest).toBeCalledWith(apiRequestDto);
+      //@ts-ignore
+      expect(TokenUtil.login).toBeCalledTimes(0);
+      //@ts-ignore
+      expect(authModel.requestAuthorizedData).toBeCalledTimes(0);
+      //@ts-ignore
+      expect(TokenUtil.setAuthorized).toBeCalledTimes(0);
+    });
+  });
+
+  it('Test requestAuthorizedData', async () => {
+    const apiRequestDto = new ApiRequestDto('/authorized', 'GET');
+    const apiResponseDto = new ApiResponseDto(true, 'data', null);
+    authModel.authorizedRequest = vi.fn(
+      () =>
+        new Promise<ApiResponseDto<any>>((resolve) => {
+          resolve(apiResponseDto);
+        }),
+    );
+
+    //@ts-ignore
+    authModel.requestAuthorizedData();
+
+    expect(authModel.authorizedRequest).toBeCalledWith({ ...apiRequestDto });
+  });
+});
