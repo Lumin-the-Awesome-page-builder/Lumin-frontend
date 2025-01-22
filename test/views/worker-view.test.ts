3e6e0ad9ae0ee233e895eb97ae7951db34a56d0c
h1l3xx <alksmlkv@gmail.com>
2025-01-22 08:12:49 +0300

    fix(lint): linter fix

--- test/views/worker-view.test.ts
+++ test/views/worker-view.test.ts
@@ -1,10 +1,10 @@
-import {expect, test} from "vitest";
-import {shallowMount} from "@vue/test-utils";
-import BubbleSortComponent from "@/components/webworker/BubbleSortComponent.vue";
-import WorkerView from "@/views/WorkerView.vue";
+import { expect, test } from 'vitest';
+import { shallowMount } from '@vue/test-utils';
+import BubbleSortComponent from '@/components/webworker/BubbleSortComponent.vue';
+import WorkerView from '@/views/WorkerView.vue';
 
 test('renders BubbleSortDemo', () => {
-    const wrapper = shallowMount(WorkerView);
-    const bubbleSortComponent = wrapper.findComponent(BubbleSortComponent);
-    expect(bubbleSortComponent.exists()).toBe(true);
+  const wrapper = shallowMount(WorkerView);
+  const bubbleSortComponent = wrapper.findComponent(BubbleSortComponent);
+  expect(bubbleSortComponent.exists()).toBe(true);
 });
